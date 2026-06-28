7482e4c335fc634a4c02b05a27bc5f04d393d0b2
github.com/users/qgbcs <36606669+qgbcs@users.noreply.github.com>
2026-06-28 10:42:11 +0000

    TARGET_CHIP=GD32F103RCT6 ./build.sh

--- Src/main.c
+++ Src/main.c
@@ -1,25 +1,3 @@
-/*
-* This file is part of the hoverboard-firmware-hack project.
-*
-* Copyright (C) 2017-2018 Rene Hopf <renehopf@mac.com>
-* Copyright (C) 2017-2018 Nico Stute <crinq@crinq.de>
-* Copyright (C) 2017-2018 Niklas Fauth <niklas.fauth@kit.fail>
-* Copyright (C) 2019-2020 Emanuel FERU <aerdronix@gmail.com>
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <stdio.h>
 #include <stdlib.h> // for abs()
 #include "stm32f1xx_hal.h"
